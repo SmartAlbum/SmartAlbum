7600bb1d71a49ace1e903f6b0ebd000edce1049c
yongry <527068687@qq.com>
2015-11-19 19:07:48 +0800

    first commit

---
+++ SmartAlbum/controller/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SmartAlbum
+//
+//  Created by JessieYong on 15/11/19.
+//  Copyright © 2015年 JessieYong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
