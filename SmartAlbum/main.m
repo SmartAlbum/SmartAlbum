1cfec515f94d36ff2da523a7288c1ca6c356cc2b
yongry <527068687@qq.com>
2015-11-19 17:01:03 +0800

    Initial Commit

---
+++ SmartAlbum/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SmartAlbum
+//
+//  Created by JessieYong on 15/11/19.
+//  Copyright © 2015年 JessieYong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
